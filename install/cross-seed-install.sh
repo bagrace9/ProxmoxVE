5d2475d69f97de17d94363d46cfe321b2f0d4f78
bagrace9 <bagrace9@gmail.com>
2026-07-30 12:56:14 -0400

    trying different node version

--- install/cross-seed-install.sh
+++ install/cross-seed-install.sh
@@ -21,8 +21,8 @@ $STD apt install -y \
   libsqlite3-dev
 msg_ok "Installed Dependencies"
 
-NODE_VERSION="26" setup_nodejs
-ensure_dependencies build-essential pkg-config python3-dev python-is-python3 libsqlite3-dev
+NODE_VERSION="24" setup_nodejs
+ensure_dependencies build-essential pkg-config python3-dev libsqlite3-dev
 
 msg_info "Setup Cross-Seed"
 # Use --unsafe-perm so npm lifecycle scripts can run correctly inside the container.
